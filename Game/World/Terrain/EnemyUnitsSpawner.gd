9fc646e03f9828d2515836566a62666209650a2c
Xydium <prodriver54@gmail.com>
2018-04-03 14:47:14 -0400

    Fixed Spawn Position

--- Game/World/Terrain/EnemyUnitsSpawner.gd
+++ Game/World/Terrain/EnemyUnitsSpawner.gd
@@ -8,9 +8,9 @@ onready var spawnable_units = _load_units()
 onready var spawners = $"../../Spawners"
 
 func _process(delta):
-	if randf() < 0.02:
+	if randf() < 0.001:
 		var helper = SpawningHelper.new(spawnable_units[0], 1, self, Vector2(-1, 0).rotated(rand_range(-.5, .5)))
-		helper.position = Vector2(500, rand_range(-200, 200))
+		helper.position = Vector2(475, rand_range(-200, 200))
 		spawners.add_child(helper)
 
 func _load_units():
